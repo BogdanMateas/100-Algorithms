8fa918f17c1bbfde8cf5f7875bf525fd78889bfc
BogdanMateas <bogdani.mateas@yahoo.com>
2019-11-26 12:17:38 +0200

    the second one improved

--- add.js
+++ add.js
@@ -1,5 +1,18 @@
-const add = (...num) => {
-  return num+= num;
+function add(...numbers) {
+  return numbers.reduce((total, element) => {
+    return (total += element);
+  });
+}
+
+const add2 = (...numbers) => {
+  let total = 0;
+  numbers.forEach(num => (total += num));
+  return total;
 };
 
-console.log(add(1, 2));
+console.log(add(1, 3));
+console.log(add(37, 3));
+console.log(add(2, 2, 2, 2, 2));
+console.log(add(2, 2));
+
+console.log(add2(2, 2));
