4205ae9b33e2363a5db441fb44c78c0a4a13ce85
BogdanMateas <bogdani.mateas@yahoo.com>
2019-11-29 13:50:05 +0200

    2 more files added

---
+++ 100AlgorithmsChallenge-master/alternatingSums/alternatingSums.js
@@ -0,0 +1,24 @@
+function alternatingSums(arr) {
+    return arr.reduce((acc, val, i) =>  {
+      acc[i % 2] = acc[i % 2] + val;
+      return acc
+    }, [0, 0]);
+  }
+  
+  console.log(alternatingSums([1,2,3,4,5,6]))
+  
+  function alternatingSums2(arr) {
+      
+      if ( arr.length === 0 ){ return []; }
+      
+      let oddSum= 0, evenSum=  0;
+   
+      for (let i = 0; i < arr.length; i++) {
+          i % 2 === 0 ? oddSum += arr[i] : evenSum += arr[i];
+      }
+      
+      return [oddSum, evenSum];
+  }
+   
+   
+  console.log(alternatingSums2([50,60,60,45,70]));
