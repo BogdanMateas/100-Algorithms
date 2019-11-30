727dfafe8dfaa47df6730674ac809921d74ce938
BogdanMateas <bogdani.mateas@yahoo.com>
2019-11-30 11:54:47 +0200

    changed

--- areEquallyStrong/areEquallyStrong.js
+++ areEquallyStrong/areEquallyStrong.js
@@ -1,6 +1,8 @@
-function areEquallyStrong(al, ar, bl, br) {
-    return al+ar-bl-br === 0? true: false;
+function areEquallyStrong(myLeft, myRight, hisLeft, hisRight) {
+    return myLeft+ myRight-hisLeft-hisRight === 0? true: false;
 }
 console.log(areEquallyStrong(10,15, 15,10)) // true
 console.log(areEquallyStrong(15,10, 15,10)) // true
 console.log(areEquallyStrong(15,10, 15,9)) // false
+console.log(areEquallyStrong(15,10, 15,12)) // false
+console.log(areEquallyStrong(15,40, 50,5)) // false
