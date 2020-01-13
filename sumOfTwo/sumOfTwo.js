a666e1801fc85bd73332b625d4563973746e7e26
BogdanMateas <bogdani.mateas@yahoo.com>
2020-01-13 15:14:08 +0200

    add files

--- sumOfTwo/sumOfTwo.js
+++ sumOfTwo/sumOfTwo.js
@@ -1,5 +1,13 @@
-function sumOfTwo(a: number[], b: number[], v: number): boolean {
+function sumOfTwo(a, b, v) {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] + b[j] === v) {
+        return true;
+      }
+    }
+  }
 
+  return false;
 }
 
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
